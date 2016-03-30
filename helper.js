cdfc89ef35118789862ea0d576ac7f2c3053922e
Larry Gordon <lgordon@psyrendust.com>
2016-03-30 15:12:24 -0700

    feat(helper.js): add feature to get next semver version suggestion

--- helper.js
+++ helper.js
@@ -3,9 +3,24 @@
 var argv = require('minimist')(process.argv.slice(2));
 var path = require('path');
 var semver = require('semver');
+var validReleaseTypes = [
+  'major',
+  'premajor',
+  'minor',
+  'preminor',
+  'patch',
+  'prepatch',
+  'prerelease'
+];
 
 if (argv.version) {
   console.log(require('./package.json').version); // eslint-disable-line
 } else if (argv.p) {
   console.log(path.resolve(__dirname)); // eslint-disable-line
+} else if (argv.inc && argv.release) {
+  if (validReleaseTypes.indexOf(argv.release) >= 0) {
+    console.log(semver.inc(argv.inc, argv.release)); // eslint-disable-line
+  } else {
+    console.log(semver.valid(argv.release)); // eslint-disable-line
+  }
 }
